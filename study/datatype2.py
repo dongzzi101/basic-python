e1ce54a5db1ab5843e25eb518c68e2a3c13353ce
dongwook <dongzzi101@gmail.com>
2025-05-22 10:44:10 +0900

    # List # tuple : 값 못바꿈 # dictionary : 키, 값 쌍으로

---
+++ study/datatype2.py
@@ -0,0 +1,34 @@
+# List
+# tuple : 값 못바꿈
+# dictionary : 키, 값 쌍으로
+
+
+my_list = [1, 2, 3]
+print(my_list)
+print(type(my_list))
+
+students = ['nana', 'kayler', 'helerty']
+print(students)
+
+for student in students:
+    print(student)
+
+students.append('meta')
+
+for student in students:
+    print(student)
+
+students[0] = 'facebook'
+for std in students:
+    print(std)
+
+my_tuple = ('a', 'b', 'c')
+print(my_tuple)
+
+# my_tuple[0] = 'C'
+
+my_dict = {'kaka': '남', 'ray': '여'}
+print(my_dict['kaka'])
+
+my_dict['kaka'] = '여'
+print(my_dict['kaka'])
