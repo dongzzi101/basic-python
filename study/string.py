52d8d91b9a18bc146d29196aeedf48a76f2b0f1f
dongwook <dongzzi101@gmail.com>
2025-05-22 10:53:13 +0900

    # 문자열 : 문자들을 나열한 것 # 큰 따옴표, 작은 따옴표로 묶음 -> 값, 순서 변경 불가

---
+++ study/string.py
@@ -0,0 +1,14 @@
+# 문자열 : 문자들을 나열한 것
+# 큰 따옴표, 작은 따옴표로 묶음 -> 값, 순서 변경 불가
+
+my_str = "김씨가족"
+print(my_str)
+print(type(my_str))
+
+my_str = '호박'
+print(my_str)
+print(type(my_str))
+
+my_str = """제스퍼\n토미\n메타"""
+print(my_str)
+print(type(my_str))
