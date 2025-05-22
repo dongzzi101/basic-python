2d47535308d944326d4f10c495ed826553c0d605
dongwook <dongzzi101@gmail.com>
2025-05-22 10:36:01 +0900

    # Numeric 숫자 # 문자 : str # boolean : true, false # type() 함수 어떤 객체 타입인지 알 수 있음

---
+++ study/datatype.py
@@ -0,0 +1,27 @@
+# Numeric 숫자
+# 문자 : str
+# boolean : true, false
+# type() 함수 어떤 객체 타입인지 알 수 있음
+
+my_int = 3
+print(my_int)
+
+my_float = 3.14
+print(my_float)
+
+print(type(my_int))
+print(type(my_float))
+
+print(my_int + my_float)
+
+my_string = 'hello'
+print(type(my_string))
+
+
+my_bool = True
+my_bool_false = False
+print(type(my_bool))
+
+
+
+
